37a0776f721b1572c16fdf118001da48192d31cb
unbrace3 <unbrace3@gmail.com>
2016-05-03 20:16:45 -0700

    screw it, back to php

--- webhook.js
+++
@@ -1,8 +0,0 @@
-webhook: function(req, res) {
-
-   if (req.query['hub.verify_token'] === 'my_token_code') {
-      res.send(req.query['hub.challenge']);
-   } else {
-      res.send('Error, wrong validation token');    
-   }
-}
