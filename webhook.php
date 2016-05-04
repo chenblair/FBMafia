8115d4c72d4d890cf68849abcec2b3e27ae28998
unbrace3 <unbrace3@gmail.com>
2016-05-04 13:21:41 -0700

    random string for hosting

--- webhook.php
+++ webhook.php
@@ -2,7 +2,19 @@
 $access_token = "EAAH9ZB08zbAwBAPv86uoe8TZBKyMxr8ZANzUlDs9ujKWdIJpHZAktTun7A7UZA8Mqq3b21a9ZA1ZC3Nb5XWzZBfvNw1pgWfyvbNIxlYuhnrQoBLl4NbhWxxwa2N2EwOPqdocyxB0HZBP9Wddfn9JW1rYOQ0dYwNB8ulkJNVcJdZCquOAZDZD";
 $verify_token = "my_access_code";
 $hub_verify_token = null;
- 
+
+function generateRandomString($length = 10) {
+	$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	$charactersLength = strlen($characters);
+	$randomString = '';
+	for ($i = 0; $i < $length; $i++) {
+		$randomString .= $characters[rand(0, $charactersLength - 1)];
+	}
+	return $randomString;
+} 
+
+$isGame = array();
+$gameHoster = array();
 $input = json_decode(file_get_contents('php://input'), true);
 $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
 $message = $input['entry'][0]['messaging'][0]['message']['text'];
@@ -20,7 +32,14 @@ $message_to_reply = '';
 } else {
     $message_to_reply = 'Huh! what do you mean?';
 }*/
-$message_to_reply = 'Mafia incoming! Stay tuned!';
+if(preg_match('[start game]', strtolower($message))) {
+	$gameID=generateRandomString();
+	$isGame[$gameID]=true;
+	$gameHoster[$gameID]=$sender;
+	$message_to_reply = $gameID;
+} else {
+	$message_to_reply = 'Mafia incoming! Stay tuned!';
+}
 //API Url
 $url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$access_token;
 //Initiate cURL.
@@ -28,12 +47,12 @@ $ch = curl_init($url);
 //The JSON data.
 
 $jsonData = '{
-    "recipient":{
-        "id":"'.$sender.'"
-    },
-    "message":{
-        "text":"'.$message_to_reply.'"
-    }
+	"recipient":{
+		"id":"'.$sender.'"
+	},
+	"message":{
+		"text":"'.$message_to_reply.'"
+	}
 }';
 //Encode the array into JSON.
 $jsonDataEncoded = $jsonData;
@@ -46,5 +65,6 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 //curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
 //Execute the request
 if(!empty($input['entry'][0]['messaging'][0]['message'])){
-    $result = curl_exec($ch);
+	$result = curl_exec($ch);
 }
+
