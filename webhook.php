f2af11de8fd93addd1647c15ee7c1cbc933872d0
unbrace3 <unbrace3@gmail.com>
2016-05-03 22:03:31 -0700

    lets try THIS

--- webhook.php
+++ webhook.php
@@ -1,56 +1,49 @@
 <?php
+$access_token = "EAAH9ZB08zbAwBAPv86uoe8TZBKyMxr8ZANzUlDs9ujKWdIJpHZAktTun7A7UZA8Mqq3b21a9ZA1ZC3Nb5XWzZBfvNw1pgWfyvbNIxlYuhnrQoBLl4NbhWxxwa2N2EwOPqdocyxB0HZBP9Wddfn9JW1rYOQ0dYwNB8ulkJNVcJdZCquOAZDZD";
+$verify_token = "my_access_code";
+$hub_verify_token = null;
+ 
 
-$challenge = $_REQUEST['hub_challenge'];
-$verify_token = $_REQUEST['hub_verify_token'];
-
-if ($verify_token === 'my_token_code') {
-echo $challenge;
+$input = json_decode(file_get_contents('php://input'), true);
+$sender = $input['entry'][0]['messaging'][0]['sender']['id'];
+$message = $input['entry'][0]['messaging'][0]['message']['text'];
+$message_to_reply = '';
+/**
+ * Some Basic rules to validate incoming messages
+ */
+if(preg_match('[time|current time|now]', strtolower($message))) {
+    // Make request to Time API
+    ini_set('user_agent','Mozilla/4.0 (compatible; MSIE 6.0)');
+    $result = file_get_contents("http://www.timeapi.org/utc/now?format=%25a%20%25b%20%25d%20%25I:%25M:%25S%20%25Y");
+    if($result != '') {
+        $message_to_reply = $result;
+    }
+} else {
+    $message_to_reply = 'Huh! what do you mean?';
 }
-
-$access_token='EAAH9ZB08zbAwBABRz7DxBQmGJOdvdhlOqurYBe3PcFaeCNVJ6ZC6WTZCx6olTkDPW3n192p36heKEriSkYGC58ZAotSBPQDZB0QbLRhnJ0OaIZBjBKn9C1B98jOKP4JHXdJtS53vcgMs4GDO02rQXIWeisNFVk50nxY1WwwIpU0gZDZD';
-
-$url='https://graph.facebook.com/v2.6/me/subscribed_apps?access_token='+access_token;
-$context = stream_context_create(array(
-    'http' => array(
-        'method' => 'POST',
-    )
-));
-$result = file_get_contents($url, false, NULL);
-if ($result === FALSE) 
-{ 
-	die('Error');
+//API Url
+$url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$access_token;
+//Initiate cURL.
+$ch = curl_init($url);
+//The JSON data.
+$jsonData = '{
+    "recipient":{
+        "id":"'.$sender.'"
+    },
+    "message":{
+        "text":"'.$message_to_reply.'"
+    }
+}';
+//Encode the array into JSON.
+$jsonDataEncoded = $jsonData;
+//Tell cURL that we want to send a POST request.
+curl_setopt($ch, CURLOPT_POST, 1);
+//Attach our encoded JSON string to the POST fields.
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+//Set the content type to application/json
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+//Execute the request
+if(!empty($input['entry'][0]['messaging'][0]['message'])){
+    $result = curl_exec($ch);
 }
-
-// Decode the response
-$responseData = json_decode($response, TRUE);
-
-// Print the date from the response
-echo $responseData;
-
-/*$user_id=10206390442655882;
-$url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAH9ZB08zbAwBABRz7DxBQmGJOdvdhlOqurYBe3PcFaeCNVJ6ZC6WTZCx6olTkDPW3n192p36heKEriSkYGC58ZAotSBPQDZB0QbLRhnJ0OaIZBjBKn9C1B98jOKP4JHXdJtS53vcgMs4GDO02rQXIWeisNFVk50nxY1WwwIpU0gZDZD';
-
-// use key 'http' even if you send the request to https://...
-$postData = array(
-    'recipient' => array('id' => $user_id),
-    'message' => array('text' => 'hello, world!')
-);
-$context = stream_context_create(array(
-    'http' => array(
-        // http://www.php.net/manual/en/context.http.php
-        'method' => 'POST',
-        'header' => "Content-Type: application/json\r\n",
-        'content' => json_encode($postData)
-    )
-));
-$result = file_get_contents($url, false, $context);
-if ($result === FALSE) 
-{ 
-	die('Error');
-}
-
-// Decode the response
-$responseData = json_decode($response, TRUE);
-
-// Print the date from the response
-echo $responseData;*/
