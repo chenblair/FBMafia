cd77c46813967a5b175a0f8447c92b769af84e4e
unbrace3 <unbrace3@gmail.com>
2016-05-03 21:30:55 -0700

    http POST to send messages

--- webhook.php
+++ webhook.php
@@ -6,3 +6,31 @@ $verify_token = $_REQUEST['hub_verify_token'];
 if ($verify_token === 'my_token_code') {
 echo $challenge;
 }
+
+$user_id=10206390442655882;
+$url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAH9ZB08zbAwBABRz7DxBQmGJOdvdhlOqurYBe3PcFaeCNVJ6ZC6WTZCx6olTkDPW3n192p36heKEriSkYGC58ZAotSBPQDZB0QbLRhnJ0OaIZBjBKn9C1B98jOKP4JHXdJtS53vcgMs4GDO02rQXIWeisNFVk50nxY1WwwIpU0gZDZD';
+
+// use key 'http' even if you send the request to https://...
+$postData = array(
+    'recipient' => array('id' => $user_id);
+    'message' => array('text' => 'hello, world!')
+);
+$context = stream_context_create(array(
+    'http' => array(
+        // http://www.php.net/manual/en/context.http.php
+        'method' => 'POST',
+        'header' => "Content-Type: application/json\r\n",
+        'content' => json_encode($postData)
+    )
+));
+$result = file_get_contents($url, false, $context);
+if ($result === FALSE) 
+{ 
+	die('Error');
+}
+
+// Decode the response
+$responseData = json_decode($response, TRUE);
+
+// Print the date from the response
+echo $responseData;
