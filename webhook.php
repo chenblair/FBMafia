73038ef710da89e9bfc40ae75ee1ac99b146d3af
unbrace3 <unbrace3@gmail.com>
2016-05-04 12:38:33 -0700

    revert again

--- webhook.php
+++ webhook.php
@@ -2,33 +2,6 @@
 $access_token = "EAAH9ZB08zbAwBAPv86uoe8TZBKyMxr8ZANzUlDs9ujKWdIJpHZAktTun7A7UZA8Mqq3b21a9ZA1ZC3Nb5XWzZBfvNw1pgWfyvbNIxlYuhnrQoBLl4NbhWxxwa2N2EwOPqdocyxB0HZBP9Wddfn9JW1rYOQ0dYwNB8ulkJNVcJdZCquOAZDZD";
 $verify_token = "my_access_code";
 $hub_verify_token = null;
-
-function message($url,$sender,$message_to_reply) {
-	$ch = curl_init($url);
-//The JSON data.
-
-	$jsonData = '{
-		"recipient":{
-			"id":"'.$sender.'"
-		},
-		"message":{
-			"text":"'.$message_to_reply.'"
-		}
-	}';
-//Encode the array into JSON.
-	$jsonDataEncoded = $jsonData;
-//Tell cURL that we want to send a POST request.
-	curl_setopt($ch, CURLOPT_POST, 1);
-//Attach our encoded JSON string to the POST fields.
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
-//Set the content type to application/json
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
-//Execute the request
-	if(!empty($input['entry'][0]['messaging'][0]['message'])){
-		$result = curl_exec($ch);
-	}
-}
  
 $input = json_decode(file_get_contents('php://input'), true);
 $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
@@ -51,4 +24,27 @@ $message_to_reply = 'Mafia incoming! Stay tuned!';
 //API Url
 $url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$access_token;
 //Initiate cURL.
-message($url,$sender,$message_to_reply);
+$ch = curl_init($url);
+//The JSON data.
+
+$jsonData = '{
+    "recipient":{
+        "id":"'.$sender.'"
+    },
+    "message":{
+        "text":"'.$message_to_reply.'"
+    }
+}';
+//Encode the array into JSON.
+$jsonDataEncoded = $jsonData;
+//Tell cURL that we want to send a POST request.
+curl_setopt($ch, CURLOPT_POST, 1);
+//Attach our encoded JSON string to the POST fields.
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+//Set the content type to application/json
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+//Execute the request
+if(!empty($input['entry'][0]['messaging'][0]['message'])){
+    $result = curl_exec($ch);
+}
