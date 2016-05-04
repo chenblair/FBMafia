5700edad480c03b140e9d001a3d9091fce15d98e
unbrace3 <unbrace3@gmail.com>
2016-05-04 12:30:06 -0700

    revert sendString v0.2

--- webhook.php
+++ webhook.php
@@ -3,7 +3,6 @@ $access_token = "EAAH9ZB08zbAwBAPv86uoe8TZBKyMxr8ZANzUlDs9ujKWdIJpHZAktTun7A7UZA
 $verify_token = "my_access_code";
 $hub_verify_token = null;
  
-
 $input = json_decode(file_get_contents('php://input'), true);
 $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
 $message = $input['entry'][0]['messaging'][0]['message']['text'];
@@ -11,7 +10,6 @@ $message_to_reply = '';
 /**
  * Some Basic rules to validate incoming messages
  */
-
 /*if(preg_match('[time|current time|now]', strtolower($message))) {
     // Make request to Time API
     ini_set('user_agent','Mozilla/4.0 (compatible; MSIE 6.0)');
@@ -22,7 +20,7 @@ $message_to_reply = '';
 } else {
     $message_to_reply = 'Huh! what do you mean?';
 }*/
-$message_to_reply = 'Mafia incoming! Stay tuned!'
+$message_to_reply = 'Mafia incoming! Stay tuned!';
 //API Url
 $url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$access_token;
 //Initiate cURL.
