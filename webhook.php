4f0183d534f12e5b077523ae5adf747c6fddcff7
unbrace3 <unbrace3@gmail.com>
2016-05-07 00:11:34 -0700

    sendMessage function v0.0

--- webhook.php
+++ webhook.php
@@ -2,26 +2,6 @@
 $access_token = "EAAH9ZB08zbAwBAPv86uoe8TZBKyMxr8ZANzUlDs9ujKWdIJpHZAktTun7A7UZA8Mqq3b21a9ZA1ZC3Nb5XWzZBfvNw1pgWfyvbNIxlYuhnrQoBLl4NbhWxxwa2N2EwOPqdocyxB0HZBP9Wddfn9JW1rYOQ0dYwNB8ulkJNVcJdZCquOAZDZD";
 $verify_token = "my_access_code";
 $hub_verify_token = null;
-
-//API Url
-$url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$access_token;
-
-# Establish db connection
-$db = pg_connect(pg_connection_string());
-if (!$db) {
-	echo "Database connection error.";
-	exit;
-}
-$query= pg_query($db, "SELECT * FROM players;");
-$row=pg_fetch_assoc($query);
-$counter=$row['userid'];
-
-$input = json_decode(file_get_contents('php://input'), true);
-$sender = $input['entry'][0]['messaging'][0]['sender']['id'];
-$message = $input['entry'][0]['messaging'][0]['message']['text'];
-$message_to_reply = '';
-
-
 function generateRandomString($length = 10) {
 	$characters = '0123456789abcdefghijklmnopqrstuvwxyz';
 	$charactersLength = strlen($characters);
@@ -31,46 +11,34 @@ function generateRandomString($length = 10) {
 	}
 	return $randomString;
 } 
-function sendMessage($recipient,$message)
-{
-	global $input;
-	//Initiate cURL.
-	$ch = curl_init($url);
-	$jsonData = '{
-		"recipient":{
-			"id":"'.$recipient.'"
-		},
-		"message":{
-			"text":"'.$message.'"
-		}
-	}';
-//Encode the array into JSON.
-	$jsonDataEncoded = $jsonData;
-//Tell cURL that we want to send a POST request.
-	curl_setopt($ch, CURLOPT_POST, 1);
-//Attach our encoded JSON string to the POST fields.
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
-//Set the content type to application/json
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
-//Execute the request
-	if(!empty($input['entry'][0]['messaging'][0]['message'])){
-		$result = curl_exec($ch);
-	}
-}
-
 //database calls
 /*$dbHost = 'ec2-54-243-243-135.compute-1.amazonaws.com';
 $dbUsername = 'd5gei6idamag9h';
 $dbPassword = 'y9AcZwkmoyyM6L41Bzk9pebGYD';
 $dbName = 'd5gei6idamag9h';
 $myPDO = new PDO('pgsql:host='+$dbHost+';dbname='+$dbName, $dbUsername, $dbName);*/
-
 # This function reads your DATABASE_URL configuration automatically set by Heroku
 # the return value is a string that will work with pg_connect
 function pg_connection_string() {
 	return "dbname=d270g74n5lg3ni host=ec2-50-16-218-45.compute-1.amazonaws.com port=5432 user=iuyrqoboxxpsri password=KuNsezJCq0rW4MeF2d2AyWKjE8 sslmode=require";
 }
+# Establish db connection
+$db = pg_connect(pg_connection_string());
+if (!$db) {
+	echo "Database connection error.";
+	exit;
+}
+$query= pg_query($db, "SELECT * FROM players;");
+$row=pg_fetch_assoc($query);
+$counter=$row['userid'];
+$input = json_decode(file_get_contents('php://input'), true);
+$sender = $input['entry'][0]['messaging'][0]['sender']['id'];
+$message = $input['entry'][0]['messaging'][0]['message']['text'];
+$message_to_reply = '';
+//API Url
+$url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$access_token;
+//Initiate cURL.
+$ch = curl_init($url);
 /**
  * Some Basic rules to validate incoming messages
  */
@@ -97,13 +65,51 @@ if(preg_match('[start game]', strtolower($message))) {
 			$hoster=$row['userid'];
 			pg_query($db, "INSERT INTO players (userid,gameid,ishost) VALUES ('$sender','$game',FALSE);");
 			$message_to_reply='You have been successfully added to game '.$game.' hosted by '.$hoster.'!';
-			sendMessage($hoster,$sender.'has just been added to your game!');
+			$jsonData = '{
+				"recipient":{
+					"id":"'.$hoster.'"
+				},
+				"message":{
+					"text":"'.$sender.'has just been added to your game!'.'"
+				}
+			}';
+//Encode the array into JSON.
+			$jsonDataEncoded = $jsonData;
+//Tell cURL that we want to send a POST request.
+			curl_setopt($ch, CURLOPT_POST, 1);
+//Attach our encoded JSON string to the POST fields.
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+//Set the content type to application/json
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+//Execute the request
+			if(!empty($input['entry'][0]['messaging'][0]['message'])) {
+				$result = curl_exec($ch);
+			}
 		}
 	}
 } else {
 	$message_to_reply = 'Mafia incoming! Stay tuned!';
 }
 //The JSON data.
-
-sendMessage($sender,$message_to_reply);
-
+$jsonData = '{
+	"recipient":{
+		"id":"'.$sender.'"
+	},
+	"message":{
+		"text":"'.$message_to_reply.'"
+	}
+}';
+//Encode the array into JSON.
+$jsonDataEncoded = $jsonData;
+//Tell cURL that we want to send a POST request.
+curl_setopt($ch, CURLOPT_POST, 1);
+//Attach our encoded JSON string to the POST fields.
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+//Set the content type to application/json
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+//Execute the request
+if(!empty($input['entry'][0]['messaging'][0]['message'])){
+	$result = curl_exec($ch);
+}
